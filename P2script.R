7c6cd6d3ff21ffc7546dbadb9299575fdafdd34e
lecroc <clecrone@compasspoint-media.com>
2017-10-08 19:45:57 -0500

    Work from 10-8-17

--- P2script.R
+++ P2script.R
@@ -26,6 +26,10 @@ keep<-c(2,7,8,23,24,25,26,27,28)
 
 d2<-d1[,keep]
 
+# Show columns I've kept
+
+names(d2)
+
 # check for missing data
 
 length(complete.cases(d2))
@@ -37,6 +41,10 @@ nrow(d2)
 # convert from factor to character
 
 d2$BGN_DATE<-as.character(d2$BGN_DATE)
+
+# look at what dates look like
+
+head(d2$BGN_DATE)
 
 # trim last 8 spaces
 
@@ -54,38 +62,65 @@ d2$BGN_DATE<-format(d2$BGN_DATE, format="%Y")
 
 names(d2)<-c("Year", "State", "Event", "Deaths", "Injuries", "PropertyDamage", "PExp", "CropDamage", "CExp")
 
+head(d2)
+
 # Look at levels of propergy damage multiplier
 
 Plvllist<-levels(d2$PExp)
 Clvllist<-levels(d2$CExp)
 
+Plvllist
+Clvllist
+
+#Convert Exponent columns to character
 
 d2$PExp<-as.character(d2$PExp)
+d2$CExp<-as.character(d2$CExp)
+
+# Convert exponent flags to numeric characters
 
 d2<-d2 %>% 
     mutate(PExp = ifelse(PExp == "H" | PExp == "h", "2", PExp)) %>%
     mutate(PExp = ifelse(PExp == "K" | PExp == "k", "3", PExp)) %>%
     mutate(PExp = ifelse(PExp == "M" | PExp == "m", "6", PExp)) %>%
     mutate(PExp = ifelse(PExp == "B" | PExp == "b", "9", PExp)) %>%
+    mutate(PExp = ifelse(PExp == "+", "0", PExp))               %>%
     mutate(CExp = ifelse(CExp == "H" | CExp == "h", "2", CExp)) %>%
     mutate(CExp = ifelse(CExp == "K" | CExp == "k", "3", CExp)) %>%
     mutate(CExp = ifelse(CExp == "M" | CExp == "m", "6", CExp)) %>%
-    mutate(CExp = ifelse(CExp == "B" | CExp == "b", "9", CExp))
+    mutate(CExp = ifelse(CExp == "B" | CExp == "b", "9", CExp)) %>%
+    mutate(CExp = ifelse(CExp == "+", "0", CExp))
 
+# Convert numeric flags to actual numbers - creates NAs for non numerics
 
 d2$PExp<-as.numeric(d2$PExp)
 d2$CExp<-as.numeric(d2$CExp)
 
-d2 <- d2 %>%
-  mutate(PExp = ifelse(is.na(PExp),0,PExp)) %>%
-  mutate(CExp = ifelse(is.na(CExp),0,CExp))
+# Convert NAs to -1
 
-d2$PropertyDamage<-d2$PropertyDamage*10^d2$PExp
-d2$CropDamage<-d2$CropDamage*10^d2$CExp
+d2 <- d2 %>%
+  mutate(PExp = ifelse(is.na(PExp),-1,PExp)) %>%
+  mutate(CExp = ifelse(is.na(CExp),-1,CExp))
+
+# Turn off scientific notation
+
+options(scipen = 999)
+
+# Convert Property Damage and Crop Damage estimates by multiplying by the exponents
+
+d2<- d2 %>%
+  mutate(PropertyDamage = ifelse(PExp == -1, 0, PropertyDamage*10^PExp)) %>%
+  mutate(CropDamage = ifelse(CExp == -1, 0, CropDamage*10^CExp))
+
+# drop exponent columns - don't need anymore
+
+keep2<-c(1,2,3,4,5,6,8)
+
+d2<-d2[,keep2]
 
 d3<-d2 %>%
   group_by(Event) %>%
-  summarize(PropertyDamage=sum(PropertyDamage), CropDamage = sum (CropDamage))
+  summarize(PropertyDamage=sum(PropertyDamage), CropDamage = sum (CropDamage), Deaths=sum(Deaths), Injuries=sum(Injuries))
 
 View(d3)
 
